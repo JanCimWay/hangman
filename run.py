fbfa0179390506f609ddfd1ad548e5e0640c6a49
JanCimWay <janis.cimermanis@gmail.com>
2024-03-08 18:14:17 +0000

    Add function hidden word

--- run.py
+++ run.py
@@ -5,5 +5,12 @@ def get_words(list):
     asked_word = list[random.randrange(len(list))]
     return asked_word
 
+def hidden_word(answer):
+    hidden = "*" * len(answer)
+    return hidden
+
 word = get_words(words_to_guess)
+hidden = hidden_word(word)
+
 print(word)
+print(hidden)
